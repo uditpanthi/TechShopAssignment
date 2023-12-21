2908c6de7ff8c6363379e97c8a2c93f098aa4028
uditpanthi <uditpanthi31@gmail.com>
2023-12-21 09:08:35 +0530

    customer managed

--- DAO/ITechShopDao.cs
+++ DAO/ITechShopDao.cs
@@ -13,5 +13,10 @@ namespace TechShopAssignment.DAO
         public void AddProduct(Products product);
         public void UpdateProduct(Products updatedProduct);
         public void RemoveProduct(int productID);
+
+
+        public void AddCustomer(Customers customer);
+        public void UpdateCustomerInfo(Customers customer);
+        public void GetCustomerDetails(int customerID);
     }
 }
--- DAO/TechShopDao.cs
+++ DAO/TechShopDao.cs
@@ -6,7 +6,7 @@ using TechShopAssignment.Utility;
 
 namespace TechShopAssignment.DAO
 {
-    public class TechShopDao: ITechShopDao
+    public class TechShopDao : ITechShopDao
     {
         public string connectionString;
         SqlCommand cmd = null;
@@ -149,5 +149,89 @@ namespace TechShopAssignment.DAO
             }
         }
 
+
+
+
+
+
+
+
+        public void AddCustomer(Customers customer)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "INSERT INTO Customers (CustomerID, FirstName, LastName, Email, Phone, Address) " +
+                               "VALUES (@CustomerID, @FirstName, @LastName, @Email, @Phone, @Address)";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CustomerID", customer.CustomerID);
+                    command.Parameters.AddWithValue("@FirstName", customer.FirstName);
+                    command.Parameters.AddWithValue("@LastName", customer.LastName);
+                    command.Parameters.AddWithValue("@Email", customer.Email);
+                    command.Parameters.AddWithValue("@Phone", customer.Phone);
+                    command.Parameters.AddWithValue("@Address", customer.Address);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void UpdateCustomerInfo(Customers customer)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "UPDATE Customers " +
+                               "SET Email = @NewEmail, Phone = @NewPhone, Address = @NewAddress " +
+                               "WHERE CustomerID = @CustomerID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CustomerID", customer.CustomerID);
+                    command.Parameters.AddWithValue("@NewEmail", customer.Email);
+                    command.Parameters.AddWithValue("@NewPhone", customer.Phone);
+                    command.Parameters.AddWithValue("@NewAddress", customer.Address);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void GetCustomerDetails(int customerID)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Customers WHERE CustomerID = @CustomerID";
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@CustomerID", customerID);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            Console.WriteLine($"Customer ID: {reader["CustomerID"]}");
+                            Console.WriteLine($"First Name: {reader["FirstName"]}");
+                            Console.WriteLine($"Last Name: {reader["LastName"]}");
+                            Console.WriteLine($"Email: {reader["Email"]}");
+                            Console.WriteLine($"Phone: {reader["Phone"]}");
+                            Console.WriteLine($"Address: {reader["Address"]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Customer not found.");
+                        }
+                    }
+                }
+            }
+
+        }
     }
 }
